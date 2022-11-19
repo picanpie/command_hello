cc6c2e2c10fcbdaef2a12e411e6fb53b862fbc52
picanpie <kysgood0@gmail.com>
2022-11-19 13:28:40 +0300

    cc

--- main.py
+++ main.py
@@ -13,9 +13,4 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-<<<<<<< HEAD
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print("bb");
-=======
- # See PyCharm help at https://www.jetbrains.com/help/pycharm/
->>>>>>> origin/master
+print("cc")
